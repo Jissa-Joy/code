a0ec1d32d6ac008579a3fe33b96485fcd61fa228
JissaJoy <40856381+jissajoy@users.noreply.github.com>
2020-07-19 08:31:11 +1000

    Add files via upload

---
+++ R.R
@@ -0,0 +1,41 @@
+install("iris")
+install.packages("dplyr")
+librar("iris")
+library("iris")
+install.packages("iris")
+options(download.file.method = "wget")
+install.packages("iris")
+library("iris")
+data()
+data(package = .packages(all.available = TRUE))
+x <- 1:7
+install.packages("swirl")
+library("swirl")
+library(swirl)
+install.packages("downloader")
+library(downloader) 
+data1 <-read.csv("femaleMiceWeights.csv")
+head(data1)
+names(data1)[1]
+names(data1)
+data1[12,2]
+x<-data1$Bodyweight
+x[11]
+length(data1)
+view(data1)
+data()
+View(data1)
+y <- data1$Bodyweight
+mean(y[13:24])
+library("")
+ls
+
+library(downloader)
+url="https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- basename(url)
+download(url,filename)
+clr
+
+
+
+
